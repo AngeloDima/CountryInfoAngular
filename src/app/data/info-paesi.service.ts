627780a23e3f6ff8173b50e28eee7418a146dc73
AngeloDima <angelodimauro91@gmail.com>
2023-06-28 18:46:51 +0200

    modellazione dati

--- src/app/data/info-paesi.service.ts
+++ src/app/data/info-paesi.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
+import { HomeInfo } from '../datiShape/home-info';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +13,30 @@ export class InfoPaesiService {
 
   url: string = "https://restcountries.com/v3.1/name/italy"
 
-  getInfo(): Observable<any[]> {
-    return this.http.get<any[]>(this.url)
+  getInfo(): Observable<HomeInfo[]> {
+    return this.http.get<HomeInfo[]>(this.url).pipe(
+      map((Response: any[]) => {
+        return Response.map(item => {
+          const info = {
+            nome: item.name.common,
+            capitale: item.capital[0],
+            moneta: item.currencies,
+            latitudine: item.latlng[0],
+            longitudine: item.latlng[1],
+            regione: item.region,
+            diminutivo: item.cioc,
+            bandiera: item.flags.svg,
+            lingua: item.languages,
+            googleMaps: item.maps.googleMaps,
+            popolazione: item.population,
+            zonaOraria: item.timezones,
+            dominio: item.tld,
+            area: item.area
+          };
+          return info as HomeInfo
+        })
+      })
+    )
 
   }
 
---
+++ src/app/datiShape/home-info.ts
@@ -0,0 +1,37 @@
+export class HomeInfo {
+
+  nome: string;
+  capitale: string;
+  moneta: string;
+  latitudine: number;
+  longitudine: number;
+  regione: string;
+  diminutivo: string;
+  bandiera: string;
+  lingua: string;
+  googleMaps: string;
+  popolazione: number;
+  zonaOraria: string;
+  dominio: string;
+  area: number;
+  constructor(nome: string, capitale: string, moneta: string, latitudine: number, longitudine: number, regione: string, diminutivo: string,
+    bandiera: string, lingua: string, googleMaps: string, popolazione: number, zonaOraria: string, dominio: string, area: number) {
+
+    this.nome = nome;
+    this.capitale = capitale;
+    this.moneta = moneta;
+    this.latitudine = latitudine;
+    this.longitudine = longitudine
+    this.regione = regione;
+    this.diminutivo = diminutivo;
+    this.bandiera = bandiera;
+    this.lingua = lingua;
+    this.googleMaps = googleMaps;
+    this.popolazione = popolazione;
+    this.zonaOraria = zonaOraria;
+    this.dominio = dominio;
+    this.area = area
+
+  }
+
+}
